965467461bbdb073455d753772a3c5ced7e0cb14
ops-pdsn <ops@pdsn.in>
2025-12-20 15:08:44 +0530

    Prisma Issue checked

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { prisma } from '@/lib/prisma'
+import { prisma } from '@/prisma'
 import bcrypt from 'bcryptjs'
 
 export const dynamic = 'force-dynamic'
